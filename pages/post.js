09b0db904b4fbb4e5ddd2eb24b918e3d708f253c
Alex Radin <alexradin1964@gmail.com>
2021-03-08 17:56:23 +0300

    2 commit

---
+++ pages/post.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+import { RichText, Date } from 'prismic-reactjs'
+import { client } from '../prismic-configuration'
+
+const Post = ({ post }) => (
+    <div>
+      <Link href="/">
+        <a>Back to blog list</a>
+      </Link>
+      {RichText.render(post.data.title)}
+      <span>{Date(post.data.date).toString()}</span>
+      {RichText.render(post.data.post_body)}
+    </div>
+)
+// prismic client.getByUID
+export async function getServerSideProps({ query }) {
+    console.log('query-----', query)
+  const post = await client.getByUID('post', query.uid)
+
+  return { props: { post } }
+}
+
+export default Post
